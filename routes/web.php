f9eb349b5f690583e60d0e1985c48a02e3b7c9c8
rezky08 <rezky.setiawan85@gmail.com>
2021-01-14 11:39:50 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['auth.redirect']], function () {
+    Route::get('/login', 'LoginController@loginForm');
+    Route::post('/login', 'LoginController@loginProcess');
+});
+Route::get('/logout', 'LoginController@logout');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::group(['prefix' => 'admin', 'middleware' => ['role:admin']], function () {
+        Route::get('/', 'Admin\HomeController@index');
+        Route::group(['prefix' => 'user'], function () {
+            Route::get('/', 'Admin\UserController@index');
+            Route::get('/add', 'Admin\UserController@create');
+            Route::post('/add', 'Admin\UserController@store');
+            Route::group(['prefix' => '{user_id}'], function () {
+                Route::get('/', 'Admin\UserController@show');
+                Route::get('/edit', 'Admin\UserController@edit');
+                Route::put('/edit', 'Admin\UserController@update');
+                Route::delete('/', 'Admin\UserController@destroy');
+            });
+        });
+
+        Route::group(['prefix' => 'role'], function () {
+            Route::get('/', 'Admin\RoleController@index');
+            Route::get('/add', 'Admin\RoleController@create');
+            Route::post('/add', 'Admin\RoleController@store');
+            Route::group(['prefix' => '{role_id}'], function () {
+                Route::get('/', 'Admin\RoleController@show');
+                Route::get('/edit', 'Admin\RoleController@edit');
+                Route::put('/edit', 'Admin\RoleController@update');
+                Route::delete('/', 'Admin\RoleController@destroy');
+            });
+        });
+    });
+    Route::group(['prefix' => '', 'middleware' => ['role:user']], function () {
+        Route::get('/', 'User\HomeController@index');
+    });
 });
