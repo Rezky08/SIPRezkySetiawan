cdc7ff8cb9acf707744df10b7cac597dbb98b805
rezky08 <rezky.setiawan85@gmail.com>
2021-01-14 11:41:40 +0700

    add model

---
+++ app/Models/Menu.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    use HasFactory;
+    public function child()
+    {
+        return $this->hasMany(Menu::class, 'parent_id', 'id');
+    }
+    public function parent()
+    {
+        return $this->belongsTo(Menu::class, 'parent_id', 'id');
+    }
+}
